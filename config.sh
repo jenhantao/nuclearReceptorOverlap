03fcb519ddaff3d6277aeacf35e6ab251a04b64b
Jenhan Tao <jenhantao@gmail.com>
2014-04-14 13:17:07 -0700

    updated config file

--- config.sh
+++ config.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
+# test variables are here
+export testTagDir="/data/mm9/ThioMac/ChIP/NuclearReceptors/NURSA_njs/BMDM.BLRP-PPARg-notx.mm9"
+export testDir="/data/mm9/ThioMac/ChIP/NuclearReceptors/NURSA_njs/NR-BLRP-Nathan/BLRP-Nathan-old"
 # environment variables are declared here
 export home="/data/home/jtao/"
 export outputDir="${home}/output/"
 
 # options for the pipeline
 #UCSC visualization (makeUCSCfile, makeBigWig.pl)
+export hub="tao"
+export genome="mm9"
 export makeUCSC=true
 export findPeaks=true
 export findMotifs=true
