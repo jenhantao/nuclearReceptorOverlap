da1fd9b32c9fe406cb95ac869c11ab751b655ce3
Jenhan Tao <jenhantao@gmail.com>
2014-05-06 15:41:34 -0700

    updates stats to include total peaks

--- calcGroupStats.py
+++ calcGroupStats.py
@@ -15,7 +15,7 @@ for line in data[1:]:
 	stat = float(tokens[5])
 	parentString = tokens[6]
 	numSubpeaks = int(tokens[7])
-	parentArray = parentString.strip().replace(path,"").replace("_ext.tsv","").replace("/","").split("|")
+	parentArray = sorted(parentString.strip().replace(path,"").replace("_ext.tsv","").replace("/","").split("|"))
 	size = int(tokens[3]) - int(tokens[2])
 	peak = (id, stat, numSubpeaks, size)
 	if len(parentArray)> 0:	
@@ -25,13 +25,30 @@ for line in data[1:]:
 		else:
 			groupHash[key].append(peak)
 
+# account for all merged regions that a subgroup appears in
+
+# sort regions by size
+groups = [(i , len(i.split(","))) for i in groupHash.keys()]
+groups.sort(key = lambda x:x[1])
+
+groupTotalPeaksHash = {k: len(groupHash[k])for k in groupHash.keys()} # key: group name, value: total number of merged region this group appears in, nonexclusive sense - merged regions are counted for multiple groups
+for group1 in groups:
+	# group is the larger group, group 2 is the smaller group
+	group1set = set(group1[0][1:-1].split(", "))
+	for group2 in groups:
+		if not group1[0] == group2[0]:
+			group2set = set(group2[0][1:-1].split(", "))
+			if len(group1set & group2set) == len(group2set):
+				groupTotalPeaksHash[group2[0]] += groupTotalPeaksHash[group1[0]]
+		
 _numInGroup = 0
 _numPeaks = 0
 _avgSubPeaks = 0
 _avgStat = 0
 _avgSize = 0
+_totalPeaks = 0
 print "### Stats Per Group ###"
-print "Group\tNum Factors in Group\tNum Merged Peaks in Group\tAvg Num Subpeaks\tAvg Stat\tAvg Size"
+print "Group\tNum Factors in Group\tNum Merged Peaks in Group\tTotal Merged Peaks\tAvg Num Subpeaks\tAvg Stat\tAvg Size"
 for key in groupHash:
 	avgSubPeaks = 0
 	avgStat = 0
@@ -45,13 +62,15 @@ for key in groupHash:
 	avgStat = float(avgStat)/float(numPeaks)
 	numInGroup = len(key.split(","))
 	avgSize = float(avgSize)/float(numPeaks)
+	totalPeaks = groupTotalPeaksHash[key]
 
 	_numInGroup += numInGroup
 	_numPeaks += numPeaks
 	_avgSubPeaks += avgSubPeaks
 	_avgStat += avgStat
 	_avgSize += avgSize
-	print key+"\t"+str(numInGroup)+"\t"+str(numPeaks)+"\t"+str(avgSubPeaks)+"\t"+str(avgStat)+"\t"+str(avgSize)
+	_totalPeaks += totalPeaks
+	print key+"\t"+str(numInGroup)+"\t"+str(numPeaks)+"\t"+str(totalPeaks)+"\t"+str(avgSubPeaks)+"\t"+str(avgStat)+"\t"+str(avgSize)
 
 print "### Overall Stats"
 _numInGroup = float(_numInGroup)/float(len(groupHash.keys()))
@@ -61,8 +80,8 @@ _avgStat = float(_avgStat)/float(len(groupHash.keys()))
 _avgSize = float(_avgSize)/float(len(groupHash.keys()))
 print "total number of peaks\t"+str(len(data)-1)
 print "number of groups:\t" + str(len(groupHash.keys()))
-print "Num Factors in Group\tNum Merged Peaks in Group\tAvg Num Subpeaks\tAvg Stat\tAvg Size"
-print str(_numInGroup)+"\t"+str(_numPeaks)+"\t"+str(_avgSubPeaks)+"\t"+str(_avgStat)+"\t"+str(_avgSize)
+print "Num Factors in Group\tAvg Num Merged Peaks in Group\tAvg Total Peaks\tAvg Num Subpeaks\tAvg Stat\tAvg Size"
+print str(_numInGroup)+"\t"+str(_numPeaks)+"\t"+str(_totalPeaks)+"\t"+str(_avgSubPeaks)+"\t"+str(_avgStat)+"\t"+str(_avgSize)
 
 print "### PEAK ASSOCIATIONS ###"
 for key in groupHash:
