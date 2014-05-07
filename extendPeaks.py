39df979c8cf4a0bd4e166ff285c3cb7cc1e98c7d
Jenhan Tao <jenhantao@gmail.com>
2014-05-07 00:17:54 -0700

    modifications to accept unannotated peak files

--- extendPeaks.py
+++ extendPeaks.py
@@ -8,7 +8,13 @@ with open(input) as f:
 	data = f.readlines()
 outputFile = open(output,'w')
 outputFile.write(data[0])
-for line in data[1:]:
+start = 0
+for line in data:
+	if line[0] == "#":
+		start+=1
+	else:
+		break
+for line in data[start:]:
 	tokens = line.split("\t")
 	start = int(tokens[2])
 	end = int(tokens[3])
