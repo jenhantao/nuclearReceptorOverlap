5ef828cd554244736173236c6bb44dc4193fed16
Jenhan Tao <jenhantao@gmail.com>
2014-05-27 14:41:30 -0700

    summarizing results of filtering

---
+++ compareFilterThresholds.sh
@@ -0,0 +1,11 @@
+#! /bin/bash 
+
+'''given paths to several nuclearReceptorOverlap outputs, creates a plot summarizing the number of merged regions per factor'''
+
+inputPath=$1
+
+for path in $inputPath
+do
+	echo $path
+done
+
