179a3b655a1e794876b1d8c4a9ac15eaafb9e6c8
Jenhan Tao <jenhantao@gmail.com>
2014-05-30 03:16:09 -0700

    initial commit of plot tag density

---
+++ plotTagDensity.py
@@ -0,0 +1,18 @@
+# given a path to a histogram file and a mapping file, plots the histogram
+
+### imports ###
+import sys 
+import math
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt 
+import numpy as np
+from matplotlib import rcParams
+import matplotlib.cm as cm
+rcParams.update({'figure.autolayout': True})
+
+plotScores
+
+
+if __name__ == "__main__":
+
