fe6bd66541cf76241b8aceb31e8beec8b8d7bb79
Jenhan Tao <jenhantao@gmail.com>
2014-06-03 21:34:33 -0700

    ontology test done

---
+++ assessGroupImportance_ontology.py
@@ -0,0 +1,217 @@
+# produces a graph visualization of a group stats file. 
+# input: group stats file, output file path
+# output: graph in DOT language format
+
+### imports ###
+import sys 
+import math
+import numpy as np
+from scipy.stats import norm
+	
+
+def readMapping(path):
+	toReturn = {}
+	with open(path) as f:
+		data = f.readlines()
+	for line in data:
+		tokens = line.strip().split("\t")
+		toReturn[tokens[0]] = tokens[1]
+	if len(toReturn.keys()):
+		return toReturn
+	else:
+		return None
+
+
+class Node:
+	def __init__(self, name = None):
+		self.parent = None
+		self.neighbors = []
+		self.components = set()
+		self.peaks = None
+		self.uniquePeaks = None
+		self.name = name
+
+def convertName(indexHash, components, factorMapping=None):
+	compList = list(components)
+	toReturn = []
+	for comp in compList:
+		toReturn.append(indexHash[comp])
+		if mapping:
+			toReturn[-1] = factorMapping[toReturn[-1]]
+	return " ".join(toReturn)
+def readOntologyMapping(ontologyPath):
+	toReturn = {}
+	with open(ontologyPath) as f:
+		data = f.readlines()
+	for line in data[1:]:
+		tokens = line.strip().split("\t")
+		toReturn[tokens[0]] = set(tokens[1:])
+	toReturn["Root"] = set()
+	return toReturn
+
+def createGraph(groupStatsFilePath, outputPath, threshold, ontologyMapping, mapping = None):
+	with open(groupStatsFilePath) as f:
+		data = f.readlines()
+	groupComponentsHash = {} # key: group name - individual factors count as groups, value: set of factors comprising that group
+	factors = set()
+	groupPeaksHash = {}
+	groupPeaksUniqueHash = {}
+	for line in data[2:]:
+		if "###" in line:
+			break
+		tokens = line.strip().split("\t")
+		group = tokens[0]
+		groupPeaksHash[group] = int(tokens[3])
+		groupPeaksUniqueHash[group] = int(tokens[2])
+		groupTokens = group[1:-1].split(", ")
+		groupComponentsHash[group] = groupTokens
+		# add in individual factors
+		for token in groupTokens:
+			factors.add(token)
+	factorIndexHash = {}
+	counter = 1
+	for factor in sorted(list(factors)):
+		factorIndexHash[factor] = str(counter)
+		counter += 1
+	# sort groups according to number of components in group
+	groupArray = [] 
+	groupIndexHash = {}
+	counter = 1
+	for key in groupComponentsHash:
+		groupArray.append((key,len(groupComponentsHash[key])))
+		groupIndexHash[key] = str(counter)
+		counter += 1
+	groupArray.sort(key=lambda x: (x[1],x[0]))
+	groupArray.reverse()
+	groupIndexHash["Root"] = "Root"
+	totalPeaks = 0
+	for factor in factors:
+		totalPeaks+=groupPeaksHash["["+factor+"]"]
+	groupPeaksHash["Root"] = totalPeaks
+	groupPeaksUniqueHash["Root"] = totalPeaks
+
+	# create initial graph
+	# create root node
+	root = Node("Root")
+	root.peaks = groupPeaksHash["Root"]
+	root.uniquePeaks = groupPeaksUniqueHash["Root"]
+	groupNodeHash = {} # key: group name, value: node
+	for i in range(len(groupArray)):
+                parentName= groupArray[i][0]
+                if not parentName in groupNodeHash:
+                        newNode = Node(parentName)
+                        newNode.components = groupComponentsHash[parentName]
+                        newNode.peaks = groupPeaksHash[parentName]
+                        newNode.uniquePeaks = groupPeaksUniqueHash[parentName]
+                        groupNodeHash[parentName] = newNode
+                parent = groupNodeHash[parentName]
+                componentsToCover = set(parent.components).copy()
+                slack = 1 # difference between the size of the parent an the size of the child
+                for j in range(len(groupArray)):
+                        if not i==j:
+                                childName = groupArray[j][0]
+                                # create new nodes if necessary
+                                if not childName in groupNodeHash:
+                                        newNode = Node(childName)
+                                        newNode.components = groupComponentsHash[childName]
+                                        newNode.peaks = groupPeaksHash[childName]
+					newNode.uniquePeaks = groupPeaksUniqueHash[childName]
+                                        groupNodeHash[childName] = newNode
+                                child = groupNodeHash[childName]
+                                if len(parent.components) == len(child.components)+slack:
+                                        if len(set(parent.components) & set(child.components)) == len(child.components):
+                                                componentsToCover = componentsToCover - set(child.components)
+                                                child.parent = parent
+                                                parent.neighbors.append(child)
+                                elif len(parent.components) == len(child.components)+slack+1:
+                                        if not componentsToCover:
+                                                break
+                                        else:
+                                                slack += 1
+
+
+	# attach nodes with no parents to the root
+	for node in groupNodeHash.values():
+		if node.parent == None and not node == root:
+			node.parent = root
+			root.neighbors.append(node)
+		node.name = convertName(factorIndexHash, node.components, mapping)
+
+	# gather the degree of overlap between each group
+	queue = [root]
+	seen = set()
+	pairSet = set ()
+	overlapSmallGroupFraction= [] #count of shared regions as a fraction of smaller group
+	while queue:
+		current = queue[0]
+		queue = queue[1:]
+		seen.add(current)
+		for neighbor in current.neighbors:
+			if not neighbor in seen and not (current,neighbor) in pairSet:
+				overlapSmallGroupFraction.append(float(current.uniquePeaks)/float(neighbor.peaks))
+				queue.append(neighbor)
+				pairSet.add((current,neighbor))
+	
+	
+	overlapSmallGroupFraction= np.log(np.array(overlapSmallGroupFraction))
+	mean = np.mean(overlapSmallGroupFraction)
+	sd = np.std(overlapSmallGroupFraction)
+	se = sd/float(len(overlapSmallGroupFraction))
+	# print graphviz file 
+        outFile = open(outputPath+"/hierarchy_overlapZtest_"+str(threshold)+".txt","w")
+        listFile = open(outputPath+"/hierarchy_overlapZtest_"+str(threshold)+".tsv","w")
+        outFile.write("graph {\n")
+        outFile.write( "ratio=1.0\n")
+        outFile.write( "dpi=50\n")
+	listFile.write("node 1\tnode 2\tstatus\tfraction of shared terms\tshared terms\n")
+	queue = [root]
+	seen = set()
+	pairSet = set ()
+	while queue:
+		current = queue[0]
+		queue = queue[1:]
+		seen.add(current)
+		for neighbor in current.neighbors:
+			if not neighbor in seen and not (current,neighbor) in pairSet:
+				logOverlap = np.log(float(current.uniquePeaks)/float(neighbor.peaks))
+				z_score = (logOverlap - mean)/se
+				p_val = norm.pdf(z_score)	
+				if p_val <= threshold:
+					if z_score < 0.0:
+						shared = ontologyMapping[current.name] & ontologyMapping[neighbor.name]
+						if len(ontologyMapping[neighbor.name]) > 0:
+							fraction = float(len(shared))/float(len(ontologyMapping[neighbor.name]))
+						else:
+							fraction = 0.0
+						outFile.write('"'+current.name+'" -- "'+neighbor.name+'" [color="red" width=1.5 label ="'+str(fraction)+'" ]\n')
+						listFile.write(current.name+"\t"+neighbor.name+"\t"+"down"+"\t"+str(p_val)+"\t"+str(fraction)+"\t"+",".join(list(shared))+"\n")
+					else:
+						shared = ontologyMapping[current.name] & ontologyMapping[neighbor.name]
+						if len(ontologyMapping[neighbor.name]) > 0:
+							fraction = float(len(shared))/float(len(ontologyMapping[neighbor.name]))
+						else:
+							fraction = 0.0
+						outFile.write('"'+current.name+'" -- "'+neighbor.name+'" [color="green" width=1.5 label ="'+str(fraction)+'" ]\n')
+						listFile.write(current.name+"\t"+neighbor.name+"\t"+"up"+"\t"+str(p_val)+"\t"+str(fraction)+"\t"+",".join(list(shared))+"\n")
+				queue.append(neighbor)
+				pairSet.add((current,neighbor))
+	outFile.write("}")
+	outFile.close()
+	listFile.close()
+	return root
+
+if __name__ == "__main__":
+	groupStatsFilePath = sys.argv[1]
+	mapping = None
+	if len(sys.argv)>5:
+		mapping = readMapping(sys.argv[5])
+	threshold = float(sys.argv[3])
+	ontologyPath = sys.argv[4]
+	ontologyMapping = readOntologyMapping(ontologyPath)
+	outputPath = sys.argv[2]
+	root = createGraph(groupStatsFilePath, outputPath, threshold, ontologyMapping, mapping)
+
+
+
+
+
--- getOntologyTerms.py
+++ getOntologyTerms.py
@@ -15,21 +15,87 @@ def findDirectories(path):
 	toReturn = {} # key: group number, tuple containing file paths for gene ontology biological process and kegg analysis
 	directories = [f for f in os.listdir(path) if not isfile(join(path,f))]
 	for directory in directories:
-		groupNumber = directory.split("_")[1]
-		directoryPath = join(path, path)
-		toReturn[groupNumber] = (directoryPath+"biological_process.txt", directoryPath+"molecular_function.txt", directoryPath+"kegg.txt")
+		groupNumber = int(directory.split("_")[1])
+		directoryPath = join(path, directory)
+		toReturn[groupNumber] = (directoryPath+"/biological_process.txt", directoryPath+"/molecular_function.txt", directoryPath+"/kegg.txt")
 	return toReturn
+
+def getGroupMappings(groupMappingPath, factorNameMapping):
+	numberFactorMapping = {}
+	with open(factorNameMapping) as f:
+		data = f.readlines()
+	for line in data:
+		tokens = line.strip().split("\t")
+		numberFactorMapping[tokens[0]] = tokens[1]
+	with open(groupMappingPath) as f:
+		data = f.readlines()
+	toReturn = {}
+	for line in data[1:]:
+		tokens = line.strip().split("\t")
+		groupNumber = tokens[1][tokens[1].index("_")+1:tokens[1].index(".")]
+		factors = []
+		for token in tokens[0][1:-1].split(", "):
+			factors.append(numberFactorMapping[token[:token.index("-")]])
+			toReturn[groupNumber] = " ".join(factors)
+	return toReturn
+		
+	
 
 # inputs: a path to ontology directories, and a mapping specifying which files correspond to which groups
 # outputs: prints to stdout, a summary of the motif data
-def getOntology(path, groupFileMapping, outPath):
+def getOntology(path, groupFileMapping, significanceThreshold, outPath, mapping):
+	logSig = np.log(significanceThreshold)
 	outFile = open(outPath, "w")
+	outFile.write("Group\tSource\tTerms\n")
+	for group in sorted(groupFileMapping.keys()):
+		toWrite = mapping[str(group)]
+		files = groupFileMapping[group]
+		biologicalFile = files[0]
+		molecularFile = files[1]
+		keggFile = files[2]
+		# read biological process file
+		with open(biologicalFile) as f:
+			data = f.readlines()
+		for line in data[1:]:
+			tokens = line.strip().split("\t")
+			term = tokens[1]
+			logP = float(tokens[3])
+			if logP <= logSig:
+				toWrite += "\t"+term
+
+		# read molecular function file
+		with open(molecularFile) as f:
+			data = f.readlines()
+		for line in data[1:]:
+			tokens = line.strip().split("\t")
+			term = tokens[1]
+			logP = float(tokens[3])
+			if logP <= logSig:
+				toWrite += "\t"+term
+
+		# read kegg file
+		with open(keggFile) as f:
+			data = f.readlines()
+		for line in data[1:]:
+			tokens = line.strip().split("\t")
+			term = tokens[1]
+			logP = float(tokens[3])
+			if logP <= logSig:
+				toWrite += "\t"+term
+		toWrite+="\n"
+		outFile.write(toWrite)
+	
+	outFile.close()
 	
 	
 
 
 if __name__ == "__main__":
 	inputPath = sys.argv[1]
-	outPath = sys.argv[2]
+	significanceThreshold = float(sys.argv[2])
+	outPath = sys.argv[3]
+	groupMappingPath = sys.argv[4]
+	factorNameMapping = sys.argv[5]
 	ontologyFiles = findDirectories(inputPath)
-	getOntology(inputPath, ontologyFiles,outPath)
+	mapping = getGroupMappings(groupMappingPath, factorNameMapping)	
+	getOntology(inputPath, ontologyFiles,significanceThreshold, outPath, mapping)
